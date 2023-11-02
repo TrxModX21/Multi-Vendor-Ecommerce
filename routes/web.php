3224709612bf30515a55142f0958bd8aebdf92de
TrxModX21 <trxcode21@gmail.com>
2023-11-02 15:41:36 +0700

    184.Create dashboards for the users

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Backend\RootController;
+use App\Http\Controllers\Backend\VendorController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -28,4 +30,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Route::get('root/dashboard', [RootController::class, 'dashboard'])->middleware(['auth'])->name('root.dashboard');
+
+Route::get('vendor/dashboard', [VendorController::class, 'dashboard'])->middleware(['auth'])->name('vendor.dashboard');
