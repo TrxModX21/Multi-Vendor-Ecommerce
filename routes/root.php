5e9ffbb07e03f2b4e96127fb6a098720af498b04
TrxModX21 <trxcode21@gmail.com>
2023-11-05 00:32:42 +0700

    210.Slider-root slider page design

--- routes/root.php
+++ routes/root.php
@@ -1,12 +1,13 @@
 <?php
+use App\Http\Controllers\Backend\SliderController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\ProfileController;
 use App\Http\Controllers\Backend\RootController;
 
-/** Root Routes */
+/** ROOT ROUTES */
 Route::get('dashboard', [RootController::class, 'dashboard'])->name('dashboard');
 
-/** Profile Routes */
+/** PROFILE ROUTES */
 Route::get('profile', [ProfileController::class, 'index'])
     ->name('profile');
 
@@ -15,3 +16,6 @@ Route::post('profile/update', [ProfileController::class, 'updateProfile'])
 
 Route::post('profile/update/password', [ProfileController::class, 'updatePassword'])
     ->name('password.update');
+
+/** SLIDER ROUTES */
+Route::resource('slider', SliderController::class);
