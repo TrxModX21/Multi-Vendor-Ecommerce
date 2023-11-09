abe9c1dd387ebdf6b1c8bfeb197b440ebc19f946
TrxModX21 <trxcode21@gmail.com>
2023-11-10 00:51:41 +0700

    256.Product-show created datas at datatable

--- app/DataTables/ProductDataTable.php
+++ app/DataTables/ProductDataTable.php
@@ -22,7 +22,66 @@ class ProductDataTable extends DataTable
     public function dataTable(QueryBuilder $query): EloquentDataTable
     {
         return (new EloquentDataTable($query))
-            ->addColumn('action', 'product.action')
+            ->addColumn('action', function ($query) {
+                $editBtn = "<a href='" . route('root.products.edit', $query->id) . "' class='btn btn-primary'><i class='far fa-edit'></i></a>";
+
+                $deleteBtn = "<a href='" . route('root.products.destroy', $query->id) . "' class='btn btn-danger ml-2 delete-item'><i class='far fa-trash-alt'></i></a>";
+
+                $moreBtn = "<div class='dropdown d-inline dropleft ml-1'>
+                    <button class='btn btn-primary dropdown-toggle' type='button' id='' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+                        <i class='fas fa-cog'></i>                    
+                    </button>
+                        <div class='dropdown-menu' x-placement='bottom-start' style='position: absolute; transform: translate3d(0px, 28px, 0px); top: 0px; left: 0px; will-change: transform;'>
+                            <a class='dropdown-item has-icon' href='#'>
+                                <i class='fas fa-heart'></i>
+                                Action                    
+                            </a>
+                            <a class='dropdown-item has-icon' href='#'>
+                                <i class='fas fa-file'></i>
+                                Another Action                    
+                            </a>
+                            <a class='dropdown-item has-icon' href='#'>
+                                <i class='fas fa-clock'></i>                    
+                                Something Else
+                            </a>
+                        </div>
+                </div>";
+
+                return $editBtn . $deleteBtn . $moreBtn;
+            })
+            ->addColumn('thumb_image', function ($query) {
+                return "<img width='100px' src='" . asset($query->thumb_image) . "' alt='no-image'></img>";
+            })
+            ->addColumn('product_type', function ($query) {
+                switch ($query->product_type) {
+                    case 'new_arrival':
+                        return '<i class="badge badge-success">New Arrival</i>';
+                    case 'featured_product':
+                        return '<i class="badge badge-warning">Featured Product</i>';
+                    case 'top_product':
+                        return '<i class="badge badge-info">Top Product</i>';
+                    case 'best_product':
+                        return '<i class="badge badge-danger">Top Product</i>';
+                    default:
+                        return '<i class="badge badge-dark">None</i>';
+                }
+            })
+            ->addColumn('status', function ($query) {
+                if ($query->status == 1) {
+                    $button = '<label class="custom-switch">
+                        <input type="checkbox" checked name="" class="custom-switch-input change-status" data-id="' . $query->id . '" />
+                        <span class="custom-switch-indicator"></span>
+                    </label>';
+                } else {
+                    $button = '<label class="custom-switch">
+                        <input type="checkbox" name="" class="custom-switch-input change-status" data-id="' . $query->id . '" />
+                        <span class="custom-switch-indicator"></span>
+                    </label>';
+                }
+
+                return $button;
+            })
+            ->rawColumns(['action', 'thumb_image', 'status', 'product_type'])
             ->setRowId('id');
     }
 
@@ -40,20 +99,20 @@ class ProductDataTable extends DataTable
     public function html(): HtmlBuilder
     {
         return $this->builder()
-                    ->setTableId('product-table')
-                    ->columns($this->getColumns())
-                    ->minifiedAjax()
-                    //->dom('Bfrtip')
-                    ->orderBy(1)
-                    ->selectStyleSingle()
-                    ->buttons([
-                        Button::make('excel'),
-                        Button::make('csv'),
-                        Button::make('pdf'),
-                        Button::make('print'),
-                        Button::make('reset'),
-                        Button::make('reload')
-                    ]);
+            ->setTableId('product-table')
+            ->columns($this->getColumns())
+            ->minifiedAjax()
+            //->dom('Bfrtip')
+            ->orderBy(0)
+            ->selectStyleSingle()
+            ->buttons([
+                Button::make('excel'),
+                Button::make('csv'),
+                Button::make('pdf'),
+                Button::make('print'),
+                Button::make('reset'),
+                Button::make('reload')
+            ]);
     }
 
     /**
@@ -62,15 +121,27 @@ class ProductDataTable extends DataTable
     public function getColumns(): array
     {
         return [
+            Column::make('id')
+                ->width(50)
+                ->addClass('text-center'),
+            Column::make('thumb_image')
+                ->width(150)
+                ->addClass('text-center'),
+            Column::make('name'),
+            Column::make('price')
+                ->width(100)
+                ->addClass('text-center'),
+            Column::make('product_type')
+                ->width(150)
+                ->addClass('text-center'),
+            Column::make('status')
+                ->width(100)
+                ->addClass('text-center'),
             Column::computed('action')
-                  ->exportable(false)
-                  ->printable(false)
-                  ->width(60)
-                  ->addClass('text-center'),
-            Column::make('id'),
-            Column::make('add your columns'),
-            Column::make('created_at'),
-            Column::make('updated_at'),
+                ->exportable(false)
+                ->printable(false)
+                ->width(200)
+                ->addClass('text-center'),
         ];
     }
 
