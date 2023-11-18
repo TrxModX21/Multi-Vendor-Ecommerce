3d7c31d2eb32c9510c3454a504460b40c6734f7d
TrxModX21 <trxcode21@gmail.com>
2023-11-18 14:51:21 +0700

    302.SProduct-making changes on edit feature

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -141,7 +141,6 @@ class ProductController extends Controller
         $product->thumb_image = empty(!$thumbImagePath) ? $thumbImagePath : $product->thumb_image;
         $product->name = $request->name;
         $product->slug = Str::slug($request->name);
-        $product->vendor_id = Auth::user()->vendor->id;
         $product->category_id = $request->category_id;
         $product->sub_category_id = $request->sub_category_id;
         $product->child_category_id = $request->child_category_id;
@@ -156,8 +155,7 @@ class ProductController extends Controller
         $product->offer_start_date = $request->offer_start_date;
         $product->offer_end_date = $request->offer_end_date;
         $product->product_type = $request->product_type;
-        $product->status = $request->status;
-        $product->is_approved = 1;
+        $product->status = $request->status;        
         $product->seo_title = $request->seo_title;
         $product->seo_description = $request->seo_description;
 
