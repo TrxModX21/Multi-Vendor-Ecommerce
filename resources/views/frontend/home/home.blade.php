e2b03a7b665d03c9aedbea681ed8b2eb3d6915bd
TrxModX21 <trxcode21@gmail.com>
2023-11-22 16:31:28 +0700

    337.AddToCart-show dynamic data at p-modal  #1

--- resources/views/frontend/home/home.blade.php
+++ resources/views/frontend/home/home.blade.php
@@ -5,29 +5,28 @@
 @endsection
 
 @section('content')
-    @include('frontend.home.sections.product-modal')
 
     @include('frontend.home.sections.banner-slider')
 
     @include('frontend.home.sections.flash-sale')
 
-    @include('frontend.home.sections.monthly-top-product')
+    {{-- @include('frontend.home.sections.monthly-top-product') --}}
 
-    @include('frontend.home.sections.brand-slider')
+    {{-- @include('frontend.home.sections.brand-slider') --}}
 
-    @include('frontend.home.sections.single-banner')
+    {{-- @include('frontend.home.sections.single-banner') --}}
 
-    @include('frontend.home.sections.hot-deals')
+    {{-- @include('frontend.home.sections.hot-deals') --}}
 
-    @include('frontend.home.sections.category-product-slider-one')
+    {{-- @include('frontend.home.sections.category-product-slider-one') --}}
 
-    @include('frontend.home.sections.category-product-slider-two')
+    {{-- @include('frontend.home.sections.category-product-slider-two') --}}
 
-    @include('frontend.home.sections.large-banner')
+    {{-- @include('frontend.home.sections.large-banner') --}}
 
-    @include('frontend.home.sections.weekly-best-item')
+    {{-- @include('frontend.home.sections.weekly-best-item') --}}
 
-    @include('frontend.home.sections.services')
+    {{-- @include('frontend.home.sections.services') --}}
 
-    @include('frontend.home.sections.blogs')
+    {{-- @include('frontend.home.sections.blogs') --}}
 @endsection
