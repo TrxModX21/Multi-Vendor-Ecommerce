6b8cf6b24623506cd3ab9995c40d7bd07da1e0a7
TrxModX21 <trxcode21@gmail.com>
2023-12-05 01:23:01 +0700

    394.working with homepage setting page #2

--- resources/views/root/home-page-setting/sections/popular-category-section.blade.php
+++ resources/views/root/home-page-setting/sections/popular-category-section.blade.php
@@ -9,8 +9,8 @@
                 <div class="row">
                     <div class="col-md-4">
                         <div class="form-group">
-                            <label for="layout">Category</label>
-                            <select name="layout" id="layout" class="form-control">
+                            <label for="main-category-1">Category</label>
+                            <select name="main-category-1" id="main-category-1" class="form-control main-category">
                                 <option value=""><---Select---></option>
 
                                 @foreach ($categories as $category)
@@ -22,24 +22,20 @@
 
                     <div class="col-md-4">
                         <div class="form-group">
-                            <label for="layout">Sub Category</label>
-                            <select name="layout" id="layout" class="form-control">
-                                <option value=""><---Select---></option>
+                            <label for="sub-category-1">Sub Category</label>
+                            <select name="sub-category-1" id="sub-category-1" class="form-control sub-category">
+                                <option value=""><---Empty---></option>
 
-                                <option value="LTR">LTR</option>
-                                <option value="RTL">RTL</option>
                             </select>
                         </div>
                     </div>
 
                     <div class="col-md-4">
                         <div class="form-group">
-                            <label for="layout">Child Category</label>
-                            <select name="layout" id="layout" class="form-control">
-                                <option value=""><---Select---></option>
+                            <label for="child-category-1">Child Category</label>
+                            <select name="child-category-1" id="child-category-1" class="form-control child-category">
+                                <option value=""><---Empty---></option>
 
-                                <option value="LTR">LTR</option>
-                                <option value="RTL">RTL</option>
                             </select>
                         </div>
                     </div>
@@ -50,7 +46,7 @@
                     <div class="col-md-4">
                         <div class="form-group">
                             <label for="layout">Category</label>
-                            <select name="layout" id="layout" class="form-control">
+                            <select name="layout" id="layout" class="form-control main-category">
                                 <option value=""><---Select---></option>
 
                                 @foreach ($categories as $category)
@@ -63,11 +59,9 @@
                     <div class="col-md-4">
                         <div class="form-group">
                             <label for="layout">Sub Category</label>
-                            <select name="layout" id="layout" class="form-control">
-                                <option value=""><---Select---></option>
+                            <select name="layout" id="layout" class="form-control sub-category">
+                                <option value=""><---Empty---></option>
 
-                                <option value="LTR">LTR</option>
-                                <option value="RTL">RTL</option>
                             </select>
                         </div>
                     </div>
@@ -75,11 +69,9 @@
                     <div class="col-md-4">
                         <div class="form-group">
                             <label for="layout">Child Category</label>
-                            <select name="layout" id="layout" class="form-control">
-                                <option value=""><---Select---></option>
+                            <select name="layout" id="layout" class="form-control child-category">
+                                <option value=""><---Empty---></option>
 
-                                <option value="LTR">LTR</option>
-                                <option value="RTL">RTL</option>
                             </select>
                         </div>
                     </div>
@@ -90,7 +82,7 @@
                     <div class="col-md-4">
                         <div class="form-group">
                             <label for="layout">Category</label>
-                            <select name="layout" id="layout" class="form-control">
+                            <select name="layout" id="layout" class="form-control main-category">
                                 <option value=""><---Select---></option>
 
                                 @foreach ($categories as $category)
@@ -103,11 +95,9 @@
                     <div class="col-md-4">
                         <div class="form-group">
                             <label for="layout">Sub Category</label>
-                            <select name="layout" id="layout" class="form-control">
-                                <option value=""><---Select---></option>
+                            <select name="layout" id="layout" class="form-control sub-category">
+                                <option value=""><---Empty---></option>
 
-                                <option value="LTR">LTR</option>
-                                <option value="RTL">RTL</option>
                             </select>
                         </div>
                     </div>
@@ -115,11 +105,9 @@
                     <div class="col-md-4">
                         <div class="form-group">
                             <label for="layout">Child Category</label>
-                            <select name="layout" id="layout" class="form-control">
-                                <option value=""><---Select---></option>
+                            <select name="layout" id="layout" class="form-control child-category">
+                                <option value=""><---Empty---></option>
 
-                                <option value="LTR">LTR</option>
-                                <option value="RTL">RTL</option>
                             </select>
                         </div>
                     </div>
@@ -130,7 +118,7 @@
                     <div class="col-md-4">
                         <div class="form-group">
                             <label for="layout">Category</label>
-                            <select name="layout" id="layout" class="form-control">
+                            <select name="layout" id="layout" class="form-control main-category">
                                 <option value=""><---Select---></option>
 
                                 @foreach ($categories as $category)
@@ -143,11 +131,9 @@
                     <div class="col-md-4">
                         <div class="form-group">
                             <label for="layout">Sub Category</label>
-                            <select name="layout" id="layout" class="form-control">
-                                <option value=""><---Select---></option>
+                            <select name="layout" id="layout" class="form-control sub-category">
+                                <option value=""><---Empty---></option>
 
-                                <option value="LTR">LTR</option>
-                                <option value="RTL">RTL</option>
                             </select>
                         </div>
                     </div>
@@ -155,11 +141,9 @@
                     <div class="col-md-4">
                         <div class="form-group">
                             <label for="layout">Child Category</label>
-                            <select name="layout" id="layout" class="form-control">
-                                <option value=""><---Select---></option>
+                            <select name="layout" id="layout" class="form-control child-category">
+                                <option value=""><---Empty---></option>
 
-                                <option value="LTR">LTR</option>
-                                <option value="RTL">RTL</option>
                             </select>
                         </div>
                     </div>
@@ -171,3 +155,79 @@
         </div>
     </div>
 </div>
+
+@push('scripts')
+    <script>
+        $(document).ready(function() {
+            $('body').on('change', '.main-category', function(e) {
+                let id = $(this).val();
+                let row = $(this).closest('.row');
+
+                $.ajax({
+                    method: 'GET',
+                    url: "{{ route('root.get-subcategories') }}",
+                    data: {
+                        id: id
+                    },
+                    success: function(data) {
+
+                        let selector = row.find('.sub-category');
+
+                        if (data.length == 0) {
+                            selector.html('<option value=""><---Empty---></option>');
+                        } else {
+                            // RESET EVERYTHING IN OPTION
+                            selector.html('<option value=""><---Select---></option>');
+
+                            // LOOP ALL DATA FROM RESPONSE
+                            $.each(data, function(i, item) {
+                                selector.append(
+                                    `<option value="${item.id}">${item.name}</option>`
+                                );
+                            });
+                        }
+                    },
+                    error: function(xhr, status, error) {
+                        console.log(error);
+                    }
+                })
+            });
+
+            // FETCH CHILD CATEGORIES
+            $('body').on('change', '.sub-category', function(e) {
+                let id = $(this).val();
+                let row = $(this).closest('.row');
+
+                $.ajax({
+                    method: 'GET',
+                    url: "{{ route('root.products.get-childcategories') }}",
+                    data: {
+                        id: id
+                    },
+                    success: function(data) {
+
+                        let selector = row.find('.child-category');
+
+                        if (data.length == 0) {
+                            selector.html('<option value=""><---Empty---></option>');
+                        } else {
+                            // RESET EVERYTHING IN OPTION
+                            selector.html('<option value=""><---Select---></option>');
+
+                            // LOOP ALL DATA FROM RESPONSE
+                            $.each(data, function(i, item) {
+                                selector.append(
+                                    `<option value="${item.id}">${item.name}</option>`
+                                );
+                            });
+                        }
+
+                    },
+                    error: function(xhr, status, error) {
+                        console.log(error);
+                    }
+                })
+            });
+        });
+    </script>
+@endpush
