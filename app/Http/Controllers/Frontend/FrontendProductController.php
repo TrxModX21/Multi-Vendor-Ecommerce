01ad937806a8acbac8660d82060ab6083d3d7254
TrxModX21 <trxcode21@gmail.com>
2024-01-06 17:03:59 +0700

    409.ProdPage-work with prod page filtering#1

--- app/Http/Controllers/Frontend/FrontendProductController.php
+++ app/Http/Controllers/Frontend/FrontendProductController.php
@@ -36,24 +36,50 @@ class FrontendProductController extends Controller
                 'category_id' => $category->id,
                 'status' => 1,
                 'is_approved' => 1
-            ])->paginate(12);
+            ])
+                ->when($request->has('range'), function ($query) use ($request) {
+                    $price = explode(';', $request->range);
+                    $from = $price[0];
+                    $to = $price[1];
+
+                    return $query->where('price', '>=', $from)->where('price', '<=', $to);
+                })
+                ->paginate(12);
         } elseif ($request->has('subcategory')) {
             $category = SubCategory::where('slug', $request->subcategory)->firstOrFail();
             $products = Product::where([
                 'sub_category_id' => $category->id,
                 'status' => 1,
                 'is_approved' => 1
-            ])->paginate(12);
+            ])
+                ->when($request->has('range'), function ($query) use ($request) {
+                    $price = explode(';', $request->range);
+                    $from = $price[0];
+                    $to = $price[1];
+
+                    return $query->where('price', '>=', $from)->where('price', '<=', $to);
+                })
+                ->paginate(12);
         } elseif ($request->has('childcategory')) {
             $category = ChildCategory::where('slug', $request->childcategory)->firstOrFail();
             $products = Product::where([
                 'child_category_id' => $category->id,
                 'status' => 1,
                 'is_approved' => 1
-            ])->paginate(12);
+            ])
+                ->when($request->has('range'), function ($query) use ($request) {
+                    $price = explode(';', $request->range);
+                    $from = $price[0];
+                    $to = $price[1];
+
+                    return $query->where('price', '>=', $from)->where('price', '<=', $to);
+                })
+                ->paginate(12);
         }
 
-        return view('frontend.pages.product', compact('products'));
+        $categories = Category::where(['status' => 1])->get();
+
+        return view('frontend.pages.product', compact('products', 'categories'));
     }
 
     public function changeListView(Request $request)
